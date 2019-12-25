9cd876920a00dc350bec4e09d505fbbf9be2a763
MarDatsko <datskomar@gmail.com>
2019-12-25 22:54:51 +0200

    Wrote a java application that division two integers, and check by null

--- src/com/homework9/Main.java
+++ src/com/homework9/Main.java
@@ -1,10 +1,12 @@
 package com.homework9;
 
 import com.homework9.task1.Task1;
+import com.homework9.task2.SafeDivision;
 
 public class Main {
 
     public static void main(String[] args) {
         new Task1().printAllCaughtException();
+        new SafeDivision().run();
     }
 }
---
+++ src/com/homework9/task2/SafeDivision.java
@@ -0,0 +1,37 @@
+package com.homework9.task2;
+
+import java.util.Scanner;
+
+public class SafeDivision {
+    private int firstNumber;
+    private int secondNumber;
+    Scanner scanner = new Scanner(System.in);
+
+    public void run(){
+        setFirstNumber();
+        setSecondNumber();
+        division();
+    }
+
+    private void division() {
+        int result;
+        try {
+            result = firstNumber / secondNumber;
+            System.out.println("Division result = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by null, change number");
+            setSecondNumber();
+            division();
+        }
+    }
+
+    private void setFirstNumber() {
+        System.out.println("Please, enter first number - ");
+        firstNumber = scanner.nextInt();
+    }
+
+    private void setSecondNumber() {
+        System.out.println("Please, enter second number - ");
+        secondNumber = scanner.nextInt();
+    }
+}
